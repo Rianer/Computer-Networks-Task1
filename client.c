db5cab97320495d728eb657a064dceb26695f3b9
Rianer <aerhan2001@gmail.com>
2021-10-24 16:21:20 +0300

    Mk 2
    Main Idea

--- client.c
+++ client.c
@@ -9,54 +9,10 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define INPUT_LIMIT 60
-#define DISCONNECT_CLIENT "disconnect"
-#define WRITE_FIFO "send_message"
-
 bool clientActive = true;
+bool sendingRequest = false;
+bool receivingAnswer = false;
 
-void disconnectClient();
-void writeFifo();
-
-const char* processInputLength(int maxChar){
-	bool limitExceded = false;
-	char c;
-	char* inputStream = malloc((sizeof(char)) * (maxChar + 1));
-	int counter = 0;
-	while( (c=fgetc(stdin)) != '\n' && c != EOF){
-		if(counter >= maxChar){
-			limitExceded = true;
-		}
-		if(counter < maxChar){
-			inputStream[counter] = c;
-		}
-		counter++;
-	}
-	if(limitExceded){
-
-		printf("--Warning: Your input exceded the limit of %d characters, the command is being ignored!\n", maxChar);
-		strcpy(inputStream,"");
-	}
-	return inputStream;
-}
-
-void checkConsoleInput(char* input){
-
-	int fd_rd, fd_wr;
-
-	printf("\nProvide input: ");
-	//scanf("%s", input);
-	//input = processInputLength(100);
-	strcpy(input, processInputLength(INPUT_LIMIT));
-	printf("Your input was: %s\n", input);
-
-	if(strcmp(input, DISCONNECT_CLIENT) == 0) {
-		disconnectClient();
-	}
-	else if(strcmp(input, WRITE_FIFO) == 0){
-		writeFifo(&fd_wr);
-	}
-}
 
 void disconnectClient(){
 	printf("Disconnecting client...\n");
@@ -64,24 +20,48 @@ void disconnectClient(){
 	clientActive = false;
 }
 
-void writeFifo(int* fd_wr){
-	if( (*fd_wr = open("myFifo2", O_WRONLY)) == -1 ){
-		perror("Open");
+void sendConsoleInput(){
+	char* inputLine = NULL;
+	size_t inputLen = 0;
+	getline(&inputLine, &inputLen, stdin);
+	inputLine[strlen(inputLine)] = '\0'; 
+
+	int fd;
+
+	if( (fd = open("myFifo2", O_WRONLY)) == -1 ){
+		perror("Open FIFO");
 		exit(-1);
 	}
 	else{
-		printf("Fifo opened for writing...\n");
-		write(*fd_wr, "Test text", 10);
+		//printf("Fifo opened for writing...\n");
+		write(fd, inputLine, strlen(inputLine) + 1);
+		//printf("Information sent: %s %ld \n", inputLine, strlen(inputLine));
 	}
+
+	close(fd);
 }
+
 
 int main(){
 
 	char userInput[60];
 	
+	sendingRequest = true;
 	while(clientActive){
-		checkConsoleInput(userInput);
+		if(sendingRequest){
+			sendConsoleInput();
+			sendingRequest = false;
+			receivingAnswer = true;
+			printf("\n\n");
+		}
+		if(receivingAnswer){
+			printf("Server feedback:\n");
+			sendingRequest = true;
+			receivingAnswer = false;
+			printf("\n\n");
+		}
 	}
+	sendConsoleInput();
 
 	printf("Client disconnected!\n");
 	return 0;
--- server.c
+++ server.c
@@ -9,53 +9,84 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define STOP_SERVER "stop_server"
-#define INPUT_LIMIT 30
-#define INITIATE_FIFO "start_fifo"
 
-bool serverRunning = true;
+
+bool serverRunning = false;
+bool waitingClient = false;
+bool respondingToClient = false;
+bool allClientsLeft = false;
+bool readyToProcessRequest = false;
+bool userOnline = false;
+bool userDenied = false;
 
 void stopServer();
 void readFifo();
+const char* readFromFile();
+const char* truncateSection();
 
-const char* processInputLength(int maxChar){
-	bool limitExceded = false;
-	char c;
-	char* inputStream = malloc((sizeof(char)) * (maxChar + 1));
-	int counter = 0;
-	while( (c=fgetc(stdin)) != '\n' && c != EOF){
-		if(counter >= maxChar){
-			limitExceded = true;
-		}
-		if(counter < maxChar){
-			inputStream[counter] = c;
-		}
-		counter++;
+void waitForClientJoin(){
+
+
+	printf("Server online, waiting for client to join...\n\n");
+
+	const char* request = readFromFile("myFifo2");
+	printf("Received from client: %s\n", request);
+	const char* userName = truncateSection(request, "login : ");
+	printf("Searching for: %s\n\n", userName);
+	int fd;
+	if( (fd = open("users.config", O_RDONLY|O_CREAT, 0777)) == -1 ){
+		perror("Open FIFO");
+		exit(-1);
 	}
-	if(limitExceded){
+	bool userFound = false;
+	bool userInvalid = false;
+	int status;
+	char parser = '.';
+	bool reachedEOF = false;
+	while(!userFound && !reachedEOF){
 
-		printf("--Warning: Your input exceded the limit of %d characters, the command is being ignored!\n", maxChar);
-		strcpy(inputStream,"");
+
+		int buffer_size = 20, count = 0;
+		char* buffer = malloc(sizeof(char)*(buffer_size + 1));
+		parser = '.';
+				while(parser != '\n' && parser != '}'){
+
+					read(fd,&parser,1);
+					if(count >= buffer_size){
+						{
+							//printf("Realocating buffer...\n");
+							char aux_buffer[buffer_size];
+							strcpy(aux_buffer, buffer);
+							buffer_size *= 2;
+							buffer = malloc(sizeof(char)*(buffer_size + 1));
+							strcpy(buffer, aux_buffer);
+						}
+					}
+		
+					if(parser != '{' && parser != '}' && parser != '\n'){
+						*(buffer+count) = parser;
+						count++;
+					}
+				}
+
+		if(parser == '}') reachedEOF = true;
+		printf("Checking for: %s\n", buffer);
+		status = strcmp(buffer, userName);
+		if(status == 0) userFound = true; else printf("No match...\n\n");
+		if(!userFound && reachedEOF) userInvalid = true;
 	}
-	return inputStream;
-}
 
-void checkConsoleInput(char* input){
-
-	int fd_rd, fd_wr;
-
-	printf("\nProvide input: ");
-	//scanf("%s", input);
-	//input = processInputLength(100);
-	strcpy(input, processInputLength(INPUT_LIMIT));
-	printf("Your input was: %s\n", input);
-
-	if(strcmp(input, STOP_SERVER) == 0) {
-		stopServer();
+	if(userFound){
+		printf("The user has been found!\n\n");
+		userOnline = true;
+		userDenied = false;
+	} 
+	if(userInvalid) {
+		printf("The username is invalid!\n\n");
+		userDenied = true;
+		userOnline = false;
 	}
-	else if(strcmp(input, INITIATE_FIFO) == 0){
-		readFifo(&fd_rd);
-	}
+	close(fd);
 }
 
 void stopServer(){
@@ -64,28 +95,65 @@ void stopServer(){
 	serverRunning = false;
 }
 
-void readFifo(int* fd_rd){
-	if( (*fd_rd = open("myFifo2", O_RDONLY)) == -1 ){
-		perror("Open error:");
-		exit(-1);
+const char* readFromFile(const char* path){
+	int counter = 0;
+	int char_num = 20;
+	char aux = '.';
+	char *buffer = malloc(sizeof(char)*(char_num + 1));
+	int fd = open(path, O_RDONLY);
+	while(aux != '\n' && aux != EOF){
+		read(fd,&aux,1);
+		//printf("%c_",aux);
+		
+		if(counter >= char_num){
+			{
+				//printf("Realocating buffer...\n");
+				char aux_buffer[char_num];
+				strcpy(aux_buffer, buffer);
+				char_num *= 2;
+				buffer = malloc(sizeof(char)*(char_num + 1));
+				strcpy(buffer, aux_buffer);
+
+			}
+		}
+		if(aux != '\n') {
+			*(buffer+counter) = aux;
+			counter++;
+		}
 	}
-	else{
-		printf("Fifo opened for reading...\n");
-		char buffer[20] = "....................";
-		/*while(buffer[5] == '.'){
-			read(*fd_rd, buffer, 10);
-		}*/
-		read(*fd_rd, buffer, 10);
-		printf("The message is: %s\n", buffer);
+	return buffer;
+}
+
+const char* truncateSection(const char* string, const char* section){
+
+	int buf_size = 20;
+	int counter = 0;
+	char* buffer = malloc(sizeof(char)*(buf_size + 1));
+
+	for(int i=strlen(section); i < strlen(string); i++){
+		if(counter >= buf_size){
+			{
+				//printf("Realocating buffer...\n");
+				char aux_buffer[buf_size];
+				strcpy(aux_buffer, buffer);
+				buf_size *= 2;
+				buffer = malloc(sizeof(char)*(buf_size + 1));
+				strcpy(buffer, aux_buffer);
+
+			}
+		}
+		*(buffer+counter) = string[i];
+		counter++;
 	}
+	return buffer;
 }
 
 int main(){
 
-	if( mkfifo("myFifo2", 0666) == -1 ){ //creating a FIFO fyle
+	if( mkfifo("myFifo2", 0666) == -1 ){ //creating a FIFO File
 
 		if(access("myFifo2", F_OK) == 0){ //checking if file is already created
-			printf("Warning: FIFO fyle has already been created!\n");
+			printf("FIFO file has been detected!\n");
 		}
 		else{ //mkfifo didn't work, handling error
 			perror("Error at Fifo");
@@ -94,12 +162,26 @@ int main(){
 		
 	}
 
-	
-	char * clientInput;
-	char consoleInput[INPUT_LIMIT];
+	//First Phase: Waiting for a client to join the server
 
-	while(serverRunning){
-		checkConsoleInput(consoleInput);
+	waitForClientJoin();
+
+
+
+	while(serverRunning){ //Phase 2: At least one client is online, serving the client
+		//checkConsoleInput(consoleInput);
+		if(waitingClient){ //Waiting for an input in the FIFO
+
+		}
+		if(readyToProcessRequest){ //Processing the request
+
+		}
+		if(respondingToClient){ //Providing the client with the result
+
+		}
+		if(allClientsLeft){ //Waiting for another client or exit the server
+
+		}
 		
 	}
 
